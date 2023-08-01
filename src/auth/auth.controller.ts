26e4e3e562aa6aca02a6bb804480d038b9920366
pong7772 <visothipong7772@gmail.com>
2023-08-01 10:13:13 +0700

    auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
@@ -7,9 +15,14 @@ import { UpdateAuthDto } from './dto/update-auth.dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
+  @Post('signup')
   create(@Body() createAuthDto: CreateAuthDto) {
     return this.authService.create(createAuthDto);
+  }
+
+  @Post('signin')
+  signIn(@Body() createAuthDto: CreateAuthDto) {
+    return this.authService.signIn(createAuthDto);
   }
 
   @Get()
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,39 @@
-import { Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { User } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UnauthorizedException } from '@nestjs/common';
 
-@Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+  async create(createAuthDto: CreateAuthDto) {
+    console.log(createAuthDto);
+    const { username, password } = createAuthDto;
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = this.userRepository.create({
+      username,
+      password: hashedPassword,
+    });
+    return await this.userRepository.save(user);
+  }
+  async signIn(createAuthDto: CreateAuthDto) {
+    const { username, password } = createAuthDto;
+    const user = await this.userRepository.findOne({
+      where: { username: username },
+    });
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      throw new UnauthorizedException('Password is incorrect');
+    }
+    return user;
   }
 
   findAll() {
